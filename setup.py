2df4a1da50be96ee07d491b722c2029b83adee30
Jonathan Wight <jwight@mac.com>
2016-07-11 22:44:09 -0700

    Update to 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='punic',
-    version='0.0.3',
+    version='0.0.4',
     url='http://github.com/schwa/punic',
     license='MIT',
     author='Jonathan Wight',
