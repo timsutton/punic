cde63d5961514ba1a4616187fac7a3bb4a9b560f
Jonathan Wight <jwight@mac.com>
2016-07-11 20:57:57 -0700

    Make work_directory exception safe.

--- punic/utilities.py
+++ punic/utilities.py
@@ -9,18 +9,23 @@ import time
 
 @contextlib.contextmanager
 def work_directory(path):
-    # type: (Path)
-
-    path = str(path)
-    saved_wd = os.getcwd()
-    os.chdir(path)
-    yield
-    os.chdir(saved_wd)
+    # type: (Union[Path, None])
+    if path:
+        path = str(path)
+        saved_wd = os.getcwd()
+        os.chdir(path)
+    try:
+        yield
+    except:
+        raise
+    finally:
+        if path:
+            os.chdir(saved_wd)
 
 
 @contextlib.contextmanager
 def timeit(task=None):
-    # type: (str)
+    # type: (Union[str, None])
     start = time.time()
     yield
     end = time.time()
