64cd92835a484b4cd08b86e4bf85bf0ca1048526
Jonathan Wight <jwight@mac.com>
2016-07-19 11:39:01 -0700

    0.1.1 - rather important fix.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='punic',
-    version='0.1.0',
+    version='0.1.1',
     url='http://github.com/schwa/punic',
     license='MIT',
     author='Jonathan Wight',
