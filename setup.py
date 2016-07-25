9e5b68ff9d9a9e2ff18fb58fbaf2a98beadbd843
Jonathan Wight <jwight@mac.com>
2016-07-25 11:25:21 -0700

    Try and be more brazen about libyaml.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ def check_libyaml():
     import subprocess
     if not subprocess.check_output(['brew', 'ls', '--versions', 'libyaml']):
         import sys
-        sys.stderr.write('Error: libyaml not installed. Use `brew install libyaml` to install it\n')
-        exit(0)
+        sys.stderr.write('Error: {}\nError: libyaml not installed. Use `brew install libyaml` to install it\nError: {}\n'.format('*' * 80, '*' * 80))
+        exit(-1)
 
 check_libyaml()
 
