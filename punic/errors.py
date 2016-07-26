db9103dd5c396b8d4b08e4894d259ebb48c02ccc
Jonathan Wight <jwight@mac.com>
2016-07-26 10:05:49 -0700

    Added a generic punic error.
    It's lazy but for purposes of a punic CLI it's fine. If anyone ever uses
    punic as a package and expects a clean error namespace it's a problem.

--- punic/errors.py
+++ punic/errors.py
@@ -13,6 +13,9 @@ class CartfileNotFound(Exception):
     def __init__(self, path):
         self.path = path
 
+class PunicRepresentableError(Exception):
+    pass
+
 
 class NoSuchRevision(Exception):
     def __init__(self, repository, revision):
@@ -26,11 +29,17 @@ def error_handling():
         yield
     except RepositoryNotClonedError:
         logger.error('Error: No locally cloned repository found. Did you neglect to run `punic checkout` first?')
+        exit(-1)
     except CartfileNotFound as e:
         logger.error('<err>Error</err>: No Cartfile found at path: <ref>{}</ref>'.format(e.path))
+        exit(-1)
     except NoSuchRevision as e:
         logger.error('<err>Error</err>: No such revision {} found in repository {}'.format(e.revision, e.repository))
         logger.error(
             'Are you sure you are using the latest bits? Try an explicit `punic fetch` or use `punic bootstrap` instead of `punic build`')
+        exit(-1)
+    except PunicRepresentableError as e:
+        logger.error(e.message)
+        exit(-1)
     except:
         raise
