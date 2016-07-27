a0efa6969c7df460099d1b8b850be2fe70e28e18
Jonathan Wight <jwight@mac.com>
2016-07-27 09:34:15 -0700

    0-1.9

--- punic/__init__.py
+++ punic/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import division, absolute_import, print_function
 
-__version__ = '0.1.8'
+__version__ = '0.1.9'
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ check_libyaml()
 
 setup(
     name='punic',
-    version='0.1.8',
+    version='0.1.9',
     url='http://github.com/schwa/punic',
     license='MIT',
     author='Jonathan Wight',
