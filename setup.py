269a2087d7a7369c6897799ee67a2bfa230e147c
Jonathan Wight <jwight@mac.com>
2016-10-04 11:03:55 -0700

    Fixed platform typo in setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     description='Clean room python implementation of a subset of Carthage functionality',
     packages=['punic'],
     keywords = ['carthage', 'build', 'xcode', 'iOS', 'cocoapods', 'cocoa', 'macOS'],
-    platforn = "MacOS X",
+    platform = "MacOS X",
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
