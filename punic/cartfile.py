c1be5043910a075c5622248ff6ab268ff1add740
Jonathan Wight <jwight@mac.com>
2016-10-31 10:52:40 -0700

    Closes #20
    We were already skipping comments. Now we're skipping comments with optional whitespace before them.
    
    Unit tests added.

--- punic/cartfile.py
+++ punic/cartfile.py
@@ -6,7 +6,7 @@ import re
 from pathlib2 import Path
 from .specification import *
 from .errors import *
-
+import six
 
 class Cartfile(object):
     def __init__(self, specifications=None, use_ssh=False, overrides=None):
@@ -15,16 +15,30 @@ class Cartfile(object):
         self.overrides = overrides
 
     def read(self, source):
-        # type: (Path)
+        """
+        >>> Cartfile().read("# This is a comment")
+        []
+        >>> Cartfile().read("    # This is a comment")
+        []
+        >>> Cartfile().read('github "schwa/SwiftUtilities" ~> 0.2.3')
+        [github "schwa/SwiftUtilities" ~> 0.2.3]
+        >>> Cartfile().read(['# This is a command', 'github "schwa/SwiftUtilities" ~> 0.2.3', '# This is a comment too'])
+        [github "schwa/SwiftUtilities" ~> 0.2.3]
+        """
 
         if isinstance(source, Path):
             if not source.exists():
                 raise CartfileNotFound(path=source)
             source = source.open().read()
 
+        if isinstance(source, six.string_types):
+            lines = [line.rstrip() for line in source.splitlines()]
+
+        if isinstance(source, list):
+            lines = source
+
         # TODO: This is of course super feeble parsing. URLs with #s in them can break for example
-        lines = [line.rstrip() for line in source.splitlines()]
-        lines = [re.sub(r'#.+', '', line) for line in lines]
+        lines = [re.sub(r'\s*#.+', '', line) for line in lines]
         lines = [line.strip() for line in lines]
         lines = [line for line in lines if line]
         self.specifications = [Specification.cartfile_string(line, use_ssh=self.use_ssh, overrides=self.overrides) for line in lines]
