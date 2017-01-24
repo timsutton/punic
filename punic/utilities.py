e16cc7704f9961662387bf5ab92c63369f720c3c
Jonathan Wight <jwight@mac.com>
2017-01-23 17:03:45 -0800

    Provide a reveal() function to show files in the finder.

--- punic/utilities.py
+++ punic/utilities.py
@@ -7,6 +7,8 @@ import os
 import time
 import logging
 import punic
+import subprocess
+from pathlib2 import Path
 
 @contextlib.contextmanager
 def work_directory(path):
@@ -27,13 +29,17 @@ def work_directory(path):
 
 @contextlib.contextmanager
 def timeit(task=None, log=None):
-
+    # type: (Union[str, None])
     if log is None:
         log = punic.current_session.config.log_timings
-
-    # type: (Union[str, None])
     start = time.time()
     yield
     end = time.time()
     if log:
         logging.info('Task \'<ref>{}</ref>\' took <echo>{:.6f}</echo> seconds.'.format(task if task else '<unnamed task>', end - start))
+
+
+def reveal(path):
+    #type: (Path)
+    subprocess.check_call(['open', str(path.parent)])
+
